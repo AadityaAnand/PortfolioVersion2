6ee0c81cb86404147fb13f7b406da16fe040ed0f
Aaditya Anand <aadityaanand271@gmail.com>
2025-10-07 09:05:26 -0400

    feat(core): add templates and URL wiring; update views and project urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse(render(request, 'core/home.html', {}))
+def about(request):
+    return HttpResponse(render(request, 'core/about.html', {}))
