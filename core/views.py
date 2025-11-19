d982ad69eb6a1ee3f0544a1cb3b5533f04cc0c8b
Aaditya Anand <aadityaanand271@gmail.com>
2025-11-18 22:25:52 -0500

    chore: commit changes

--- core/views.py
+++ core/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse(render(request, 'core/home.html', {}))
-def about(request):
-    return HttpResponse(render(request, 'core/about.html', {}))
+    return render(request, 'core/home.html')
